0b58899bdcb4dc46d83256dbcd7149332baecfd3
Mark Lawrence <mark@rekudos.net>
2024-11-24 11:35:25 +0000

    time: duration test debug

--- test/time/duration.test.c
+++ test/time/duration.test.c
@@ -124,8 +124,7 @@ static void test_to_minutes(void) {
     for (size_t i = 0; i < sizeof(to_minute_tests) / sizeof(to_minute_tests[0]); i++) {
         ToMinuteTest test = to_minute_tests[i];
         double got = dur_to_minutes(test.d);
-        (got == test.want) || printf("in %ld: want %lf, got %lf\n", test.d,test.want, got );
-        fflush(stdout);
+        fprintf(stderr,"in %ld: want %lf, got %lf\n", test.d,test.want, got );
         assert(got == test.want);
     }
     printf("OK\n");
@@ -145,7 +144,7 @@ static void test_to_hours(void) {
     for (size_t i = 0; i < sizeof(to_hour_tests) / sizeof(to_hour_tests[0]); i++) {
         ToHourTest test = to_hour_tests[i];
         double got = dur_to_hours(test.d);
-        (got == test.want) || printf("in %ld: want %lf, got %lf\n", test.d,test.want, got );
+        fprintf(stderr,"in %ld: want %lf, got %lf\n", test.d,test.want, got );
         assert(got == test.want);
     }
     printf("OK\n");
